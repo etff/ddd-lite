6e618bd1225c5a9aceea072d8103e47341707e6d
etff <wish2rich@gmail.com>
2020-07-10 11:09:57 +0900

    feature: add base entity

--- src/main/java/com/app/dddlite/member/Member.java
+++ src/main/java/com/app/dddlite/member/Member.java
@@ -1,5 +1,6 @@
 package com.app.dddlite.member;
 
+import com.app.dddlite.config.BaseEntity;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +8,7 @@ import javax.persistence.*;
 
 @Entity
 @NoArgsConstructor(access= AccessLevel.PROTECTED)
-public class Member {
+public class Member extends BaseEntity {
     @Id
     @GeneratedValue
     @Column(name = "member_id")
